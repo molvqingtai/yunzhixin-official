2d8c5b63ccfd1faa4838c45cad1b578fd6db1b8b
John Wu <molvqingtai@gmail.com>
2021-02-17 17:46:18 +0800

    feat: Navigation Finished

--- src/views/Home/components/Navigation.tsx
+++ src/views/Home/components/Navigation.tsx
@@ -7,43 +7,57 @@ import React, {
   useEffect
 } from 'react'
 
+interface ButtonProps extends HTMLProps<HTMLButtonElement> {
+  children: string
+}
 const Button = forwardRef<HTMLButtonElement, ButtonProps>(
   (props: ButtonProps, ref) => {
-    const { children, isActive, onClick } = props
-    const activeClass = isActive ? 'bg-theme' : ''
+    const { children, onClick } = props
     return (
       <button
         ref={ref}
         onClick={onClick}
-        className={`py-1 px-5 rounded-full cursor-pointer focus:outline-none text-white border-2 border-transparent hover:border-theme ${activeClass}`}
+        className="focus:outline-none relative"
       >
-        {children}
+        <span className="transition ease-out duration-300 flex py-1 px-5 rounded-full focus:outline-none text-white border-2 border-transparent hover:border-theme">
+          {children}
+        </span>
       </button>
     )
   }
 )
 Button.displayName = 'Button'
 
-const ShadowButton: FC = () => {
-  return <button></button>
+interface ShadowButtonProps extends HTMLProps<HTMLButtonElement> {
+  rect: ClientRect
+}
+const ShadowButton: FC<ShadowButtonProps> = ({ rect }: ShadowButtonProps) => {
+  const { width, height, top, left } = rect
+  return (
+    <button
+      style={{ width, height, top, left }}
+      className="fixed transition-all ease-out duration-700 py-1 px-5 rounded-full cursor-pointer focus:outline-none text-white border-2 border-transparent  bg-theme"
+    ></button>
+  )
 }
 ShadowButton.displayName = 'ShadowButton'
-interface ButtonProps extends HTMLProps<HTMLButtonElement> {
-  children: string
-  isActive: boolean
-}
 
 interface NavigationProps extends HTMLProps<HTMLDivElement> {
   menus: string[]
 }
+
 const Navigation: FC<NavigationProps> = ({ menus }: NavigationProps) => {
   const [activeButtonIndex, setActiveButtonIndex] = useState(0)
+  const [activeButtonRect, setActiveButtonRect] = useState({})
   const buttonRefs = useRef<HTMLButtonElement[]>([])
   const handleClick = (index: number): void => setActiveButtonIndex(index)
 
   useEffect(() => {
-    console.log(buttonRefs)
-  }, [])
+    const rect = buttonRefs.current
+      .find((_, index) => index === activeButtonIndex)!
+      .getBoundingClientRect()
+    setActiveButtonRect(rect)
+  }, [activeButtonIndex])
 
   return (
     <div
@@ -53,13 +67,12 @@ const Navigation: FC<NavigationProps> = ({ menus }: NavigationProps) => {
       <div className="container mx-auto h-full flex items-center">
         <div className="text-white">云之信</div>
         <div className="flex flex-1 justify-around">
-          <ShadowButton></ShadowButton>
+          <ShadowButton rect={activeButtonRect as ClientRect}></ShadowButton>
           {menus.map((text, index) => (
             <Button
               ref={(ref: HTMLButtonElement) =>
                 (buttonRefs.current[index] = ref)
               }
-              isActive={activeButtonIndex === index}
               onClick={() => handleClick(index)}
               key={index}
             >
