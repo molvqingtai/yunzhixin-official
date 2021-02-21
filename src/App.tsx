b5b663a2fded6b88b7d43477d69297fa6064466b
John Wu <molvqingtai@gmail.com>
2021-02-22 02:13:43 +0800

    fix: github page route path

--- src/App.tsx
+++ src/App.tsx
@@ -2,14 +2,15 @@ import React from 'react'
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
 
 import Home from './views/Home/Index'
-import Notfound from './views/404/Index'
+// import Notfound from './views/404/Index'
 
 const App = (): JSX.Element => {
   return (
     <Router>
       <Switch>
-        <Route exact path={['/', '/home']} component={Home}></Route>
-        <Route component={Notfound}></Route>
+        {/* <Route exact path={['/', '/home']} component={Home}></Route> */}
+        <Route component={Home}></Route>
+        {/* <Route component={Notfound}></Route> */}
       </Switch>
     </Router>
   )
